a0cfd8d0d028c3624df73a03cc464beda1095fca
rahul-v22 <rahul.vangala@codexbox.com>
2022-08-09 12:37:55 +0530

    changes

--- src/Redux/action_types/auth_types.js
+++
@@ -1,9 +0,0 @@
-const AuthTypes = {
-    
-    LOGIN_REQUEST: "LOGIN_REQUEST",
-    LOGIN_SUCCESS: "LOGIN_SUCCESS",
-    LOGOUT_REQUEST: "LOGOUT_REQUEST",
-    LOGOUT_SUCCESS: "LOGOUT_SUCCESS",
-};
-
-export { AuthTypes };
--- src/Redux/action_types/loader_types.js
+++
@@ -1,6 +0,0 @@
-const LoaderTypes = {
-  LOADER_START: "LOADER_START",
-  LOADER_STOP: "LOADER_STOP",
-};
-
-export { LoaderTypes };
