a8fe81268a2db6b1a36c0b4acb34f4ff841487c6
rahul-v22 <rahul.vangala@codexbox.com>
2022-08-09 12:41:39 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Store from '../src/Redux/store'
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Router from './AppRouter/Routes';
-import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={Store}>
-  <Router />
-</Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
