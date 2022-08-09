fccfc44aae40def77111d5f9f431bab009bf125f
venky106 <venkateshwarlu.kethi@codexbox.com>
2022-08-09 18:37:22 +0530

    show the data in the table through axios.

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App';
+import TableAxios from "./Venkateswarlu/tableRender_axois"
 
 
 
@@ -11,9 +12,7 @@ function AppRouter() {
         <BrowserRouter>
             <Routes>
                 <Route path='/app' element={<App/>}/>
-                
-                
-
+                <Route path="venkteswarlu/tableaxios" element={<TableAxios></TableAxios>}/>
             </Routes>
         </BrowserRouter>
     )
